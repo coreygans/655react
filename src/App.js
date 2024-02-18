ec763d11cd450db947641900254974bb8085cab9
Corey <coreygans@gmail.com>
2024-02-18 08:11:55 -0600

    Filter buttons work

--- src/App.js
+++ src/App.js
@@ -6,9 +6,26 @@ import Todo from "./components/Todo";
 import { useState } from "react";
 import { nanoid } from "nanoid";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
-  const taskList = tasks?.map((task) => (
+  const [filter, setFilter] = useState("All");
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
+  const taskList = tasks.filter(FILTER_MAP[filter]).map((task) => (
     <Todo
       id={task.id}
       name={task.name}
@@ -33,18 +50,18 @@ function App(props) {
     setTasks(updatedTasks);
   }
 
-  function deleteTask(id){
+  function deleteTask(id) {
     const remainingTasks = tasks.filter((task) => id !== task.id);
     setTasks(remainingTasks);
   }
 
-  function editTask(id, newName){
+  function editTask(id, newName) {
     const editedTaskList = tasks.map((task) => {
-      if (id === task.id){
-        return{...task, name: newName};
+      if (id === task.id) {
+        return { ...task, name: newName };
       }
       return task;
-    })
+    });
     setTasks(editedTaskList);
   }
 
@@ -54,11 +71,7 @@ function App(props) {
     <div className="todoapp stack-large">
       <h1>Todo List</h1>
       <Form addTask={addTask} />
-      <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
-      </div>
+      <div className="filters btn-group stack-exception">{filterList}</div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
         className="todo-list stack-large stack-exception"

518f3fa2ecc49c9ac544ef0e65967372e206eadb
Corey <coreygans@gmail.com>
2024-02-18 07:50:23 -0600

    Editing works

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App(props) {
       key={task.id}
       toggleTaskCompleted={toggleTaskCompleted}
       deleteTask={deleteTask}
+      editTask={editTask}
     />
   ));
   function addTask(name) {
@@ -35,6 +36,16 @@ function App(props) {
   function deleteTask(id){
     const remainingTasks = tasks.filter((task) => id !== task.id);
     setTasks(remainingTasks);
+  }
+
+  function editTask(id, newName){
+    const editedTaskList = tasks.map((task) => {
+      if (id === task.id){
+        return{...task, name: newName};
+      }
+      return task;
+    })
+    setTasks(editedTaskList);
   }
 
   const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
